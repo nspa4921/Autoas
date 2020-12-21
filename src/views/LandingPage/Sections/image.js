ed1d882514c79d01d976e0de39a8a27558dc223e
Nemo <nemo.spaske@live.com>
2020-12-21 13:24:46 +0100

    Grid added to image

--- src/views/LandingPage/Sections/image.js
+++ src/views/LandingPage/Sections/image.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import image from 'assets/img/ClipartKey_242125.png';
+import GridContainer from 'components/Grid/GridContainer';
+import GridItem from 'components/Grid/GridItem';
 
 
   class Image extends Component {
     render(){
       return(
-        <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
         <img style={{width: '500px'}} className="picture" src={image} alt="Tjeneste"/>
-        </div>
+        </GridItem>
+        </GridContainer>
      );
     }
   }
