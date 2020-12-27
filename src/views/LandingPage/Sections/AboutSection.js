1caa0925793c8a071dd7ab4463f16ec951d4dc07
Nemo <nemo.spaske@live.com>
2020-12-27 16:52:41 +0100

    before build

--- src/views/LandingPage/Sections/AboutSection.js
+++ src/views/LandingPage/Sections/AboutSection.js
@@ -11,13 +11,14 @@ const useStyles = makeStyles(styles);
 
 export default function AboutSection() {
   const classes = useStyles();
+
   return ( 
     <div className="App" id="about" style={{paddingTop: '120px'}}> 
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
           <h2 className={classes.title} align="center">OM OS</h2>
           <div className={classes.description}>
-          <h5 className="explanation" align="center" style={{ fontSize: 18}}>Det uafhængige selskab blev grundlagt i 2019. Det kom som resultatet af mange års erfaring i bilbranchen, 
+          <h5 className="explanation"  align="center" style={{ fontSize: 18}}>Det uafhængige selskab blev grundlagt i 2019. Det kom som resultatet af mange års erfaring i bilbranchen, 
           først som en hobby, og derefter som professionelt arbejde.  
           <br></br>
           Upåklagelig kvalitet kan ses gennem det vellykkede samarbejde med biludstillings saloner hos <strong>AUDI, WV, TOYOTA</strong>, samt med et betydeligt antal privatpersoner. De er blev erhvervet, til min store tilfredshed, takket være anbefalingerne fra tilfredse kunder. 
