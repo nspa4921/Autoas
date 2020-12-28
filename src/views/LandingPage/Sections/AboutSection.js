a4d5cc660d13c7e36f782dbdc2d1bb387237b7bd
Nemo <nemo.spaske@live.com>
2020-12-28 15:26:09 +0100

    app.css removed

--- src/views/LandingPage/Sections/AboutSection.js
+++ src/views/LandingPage/Sections/AboutSection.js
@@ -5,7 +5,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
-import "assets/css/App.css";
 
 const useStyles = makeStyles(styles);
 
